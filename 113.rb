d5d16631f5c219928b3b1e6ce60a555cc0b9387f
Factorkevin <factorkevin24@gmail.com>
2026-03-04 13:59:18 +1000

    113.rb

---
+++ 113.rb
@@ -0,0 +1,24 @@
+# QUESTION 1
+# Write code to store 3 different numbers in three different variables.
+# Then write the code to print the three variables added together.
+
+puts kobe = "24"
+puts jordan = "23"
+puts rose = "1"
+puts "kobe" + "jordan" + "rose"
+
+# QUESTION 2
+# If you run the code below, it will crash due to invalid variable names. 
+# Find the invalid variable names and delete the special characters 
+# (be sure to leave the special characters in the valid variable names alone).
+
+bob7 = 50
+bob = 50
+bob = 50
+@bob = 50
+bob_ = 50
+_bob = 50
+bob = 50
+$bob = 50
+
+
