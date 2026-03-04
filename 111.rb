3c45216aef654f583deec8fb3a698bb43529757b
Factorkevin <factorkevin24@gmail.com>
2026-03-04 12:38:26 +1000

    111.rb

---
+++ 111.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+puts 4 + 2
+puts 30 - 5
+puts 12 * 4
+puts 983.0 / 3.0 
+
+24 * 8 Puts 
+# If printing anything out, "puts" has to be in the beginning and lowercased since ruby is a case sensitive language. 
+# kevinfactor@Kevins-Air prework-exercises % ruby 111.rb
+111.rb:16: syntax error, unexpected tCONSTANT, expecting end-of-input
