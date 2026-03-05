ebf65bfd34f353f9db91869cd395a3330c5d14ce
Factorkevin <factorkevin24@gmail.com>
2026-03-05 14:35:35 +1000

    121.rb

---
+++ 121.rb
@@ -0,0 +1,14 @@
+# QUESTION 1
+# Write example code to demonstrate three string methods you've never 
+# worked with before from the Ruby documentation. 
+# Write a comment for each method describing how it works in your own words.
+
+puts "goodmorning".length #In this method "length" will count the number of words within the string 
+puts 2.98721 .round #In this method "round" it rounds the number up to the nearest integer. 
+puts "goodnight".upcase #In this method it takes the word and turns everything into uppercased letters. 
+
+# QUESTION 2
+# Write example code that crashes using an integer method from the Ruby documentation. 
+# Write a comment describing the error message in your own words.
+
+puts 08.24 .upcase #This code crashes because we are using a method that does not go with the type of variable we are trying to associate with.  
