6a9ac8bf8bdc487e17eb18263c45ac74fe4d24cc
Factorkevin <factorkevin24@gmail.com>
2026-03-24 16:05:01 +1000

    Add exercise solutions

---
+++ 211.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#Yes, I was able to easily solve the problem from memory
+#I will start working more on vraibles 
+
+# Write a program that uses a variable to store a number, then prints that number plus 15.
+
+candy = 20
+gum = 15
+ puts "sum of sweets:", 20 + 15
