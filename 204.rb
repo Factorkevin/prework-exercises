82d80d6a502f984f098d9967bbfd78409b9358e1
Factorkevin <factorkevin24@gmail.com>
2026-03-24 16:00:07 +1000

    Create 204.rb

---
+++ 204.rb
@@ -0,0 +1,6 @@
+# QUESTION 1
+# Paste this link in your browser to make a copy of the Career Inventory Google document: 
+# https://docs.google.com/document/d/1MMO_Z7hJWEuXZ28pYFHrWp0wm-UIZQZiBpidkBSwm3w/edit?usp=sharing   
+# Fill in the answers for each question in the Career Inventory.
+# Change the sharing settings of the document to "Anyone with the link can view" 
+# Copy the link to your Career Inventory Google document and paste it below.
