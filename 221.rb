bc2c4be9ad2f6cf6093b8587a668d87b623f4178
Factorkevin <factorkevin24@gmail.com>
2026-03-26 15:15:54 +1000

    Add exercise solutions

---
+++ 221.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Write a program that uses a variable to store a word, then prints that word in reverse order.
+question = "do you know who I am?"
+puts question.reverse 
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# In this code I did have a difficult time figuring out what the code for putting the word in reverse is 
+#I will continue to keep practicing my variables and figure out more different ways to code these variables with shortcuts 
+
